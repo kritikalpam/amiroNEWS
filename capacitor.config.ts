a74ee30407e303aa4ce28114e71fdda84c20b2fe
Kritikalpam Technologies <kritikalpam@gmail.com>
2025-07-16 18:39:10 +0000

    npx cap init "amiroNEWS" "com.amiro.android.amironews" --web-dir "out" r

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.amiro.android.amironews',
+  appName: 'amiroNEWS',
+  webDir: 'out',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
