42b312511769c2b547aabdc60286410a485fb8d7
Kritikalpam Technologies <kritikalpam@gmail.com>
2025-07-17 16:12:23 +0000

    create code iso and android along with web app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,15 @@
+
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.amiro.android.amironews',
   appName: 'amiroNEWS',
   webDir: 'out',
-  server: {
-    androidScheme: 'https'
+  bundledWebRuntime: false,
+  plugins: {
+    PushNotifications: {
+      presentationOptions: ["badge", "sound", "alert"],
+    },
   },
   cordova: {}
 };
