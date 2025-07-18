55a601acfa03185af969cd2b2822e94d1cf1a9f3
Kritikalpam Technologies <kritikalpam@gmail.com>
2025-07-18 04:41:30 +0000

    splash screen not visible

--- android/app/src/main/java/com/amiro/android/amironews/MainActivity.java
+++ android/app/src/main/java/com/amiro/android/amironews/MainActivity.java
@@ -3,6 +3,7 @@ import android.os.Bundle;
 
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
+import com.getcapacitor.community.splashscreen.SplashScreen;
 
 import java.util.ArrayList;
 
@@ -10,5 +11,12 @@ public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    // Initializes the Bridge
+    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+      // Additional plugins you've installed go here
+      // Ex: add(TotallyAwesomePlugin.class);
+      add(SplashScreen.class);
+    }});
   }
 }
