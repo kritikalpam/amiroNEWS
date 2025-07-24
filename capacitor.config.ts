605bded1efe02772fa1baaf81f3209825dcf83c5
Kritikalpam Technologies <kritikalpam@gmail.com>
2025-07-24 15:52:53 +0000

    app name amiroNEWS com.amiro.news for both ios and android

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.amiro.news',
+  appName: 'amiroNEWS',
+  webDir: 'out',
+  bundledWebRuntime: false,
+};
+
+export default config;
