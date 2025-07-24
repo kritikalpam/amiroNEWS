06d8bbef0a57cc29f9408fbd4454f182d2ad56d2
Kritikalpam Technologies <kritikalpam@gmail.com>
2025-07-24 18:37:00 +0000

    Your project Project name amiroNEWS Project ID amiro-news Project number

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCICE5w6owhqEoFr55xlYwf4hZcno-txL8",
   authDomain: "amiro-news.firebaseapp.com",
   projectId: "amiro-news",
-  storageBucket: "amiro-news.appspot.com",
+  storageBucket: "amiro-news.firebasestorage.app",
   messagingSenderId: "849105130428",
   appId: "1:849105130428:web:c549277a3dfcd1860911b9"
 };
