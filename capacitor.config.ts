bd8d5492cff2ae7d721fe9ff8fcb8f0ea3a01781
Kritikalpam Technologies <kritikalpam@gmail.com>
2025-07-26 12:55:24 +0000

    notification should open in app not in browser

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,6 +5,15 @@ const config: CapacitorConfig = {
   appName: 'amiroNEWS',
   webDir: 'out',
   bundledWebRuntime: false,
+  plugins: {
+    OneSignal: {
+      // Prevents the default behavior of opening URLs in an external browser
+      // when a notification is clicked.
+      disableConsentRequirement: false,
+      canRequestPopupForPushNotifications: true,
+      launchURLsInApp: true,
+    },
+  },
 };
 
 export default config;
